e7025d1bb871906378b2dba6088454f5cdbd4dc9
Jer'Maine Montiel <jermaine.m.jones@outlook.com>
2017-01-26 00:08:15 -0600

    tested. working.
    removed the two modules and just moved the code back to the app.js file.

--- src/app.js
+++ src/app.js
@@ -3,30 +3,125 @@ const express = require('express');
 const bodyParser = require('body-parser'); //auto-parses the payload
 const requestModule = require('request'); //for creating http requests
 
-const bufferModule = require('./buffer-module.js');
-const slackModule = require('./slack-module.js');
-
 const app = express();
 const PORT = process.env.PORT || 4400;
 app.use(bodyParser.json()); //parse all payloads
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //insert your Slack token here, or through Heroku
-const SLACK_TOKEN = process.env.SLACK_TOKEN || 'xxxxxxxxxxx';
+const SLACK_TOKEN = process.env.SLACK_TOKEN || 'L9ADY964Q9BrGESFFw2VJHJH';
 
 //insert your Buffer client ID, or through Heroku
 const BUFFER_CLIENT_ID = process.env.BUFFER_CLIENT_ID || '58842b27942c4e69320f29a2';
 //insert your Buffer client secret, or through Heroku
 const BUFFER_CLIENT_SECRET = process.env.BUFFER_CLIENT_SECRET || 'e715b31d629eecb339efe09c902e7479';
-//insert your Buffer redirect uri, or through Heroku
-const BUFFER_REDIRECT_URI = process.env.BUFFER_REDIRECT_URI || 'http://6f0fed8d.ngrok.io/redirect';
-/* in the /redirect route, the app will receive access code after authorization, but you'll need to switch it out with this access_token. Check the Buffer API docs for more details: https://buffer.com/developers/api/oauth */
+//inser your Buffer access token, or through Heroku
 const BUFFER_ACCESS_TOKEN = process.env.BUFFER_ACCESS_TOKEN || "1/0a514d534c5aa60ef3fa479e3c74a3c8";
 //object of profile username:profile_id pairs by service, for use in the Buffer request
 const PROFILE_IDS = process.env.BUFFER_PROFILE_IDS ||
 {
-  'twitter': {
-    'talkdatcode': ''
+  'twitter': ['talkdatcode']
+};
+
+/*
+getBufferProfileIDs method
+  sends a GET request to Buffer, returning an array of the user's profiles.
+    returns the completed array
+*/
+const getBufferProfileIDs = (callback) => {
+  let ids = [];
+  let url = 'https://api.bufferapp.com/1/profiles.json?access_token=' + BUFFER_ACCESS_TOKEN;
+  requestModule.get(url, (err, res, body) => {
+    if(err)
+    {
+      console.log('getBufferProfileIDs(): there was an error.');
+      callback(err);
+    }
+    else if (res.statusCode !== 200){
+      console.log('getBufferProfileIDs(): Something happened.');
+      callback(null, res, body);
+    }
+    else{
+      console.log('profiles received.');
+      let resData = JSON.parse(body);
+
+      for( let i = 0; i < resData.length; i++)
+      {
+        for( let service in PROFILE_IDS )
+        {
+          for( let j = 0; j < PROFILE_IDS[service].length; j++ )
+          {
+            if(resData[i].service === service )
+            {
+              if( resData[i].service_username === PROFILE_IDS[service][j] ) { ids.push(resData[i].id); }
+              console.log(ids);
+            }
+          }
+        }
+      }
+    }
+    callback(ids);
+  });
+};
+
+/*
+bufferRequest method
+  sends a POST request to Buffer, to create a new update to the profile of your choice.
+    - see the Buffer API docs for the parameters: https://buffer.com/developers/api/updates#updatescreate
+    parameters:
+      tweet - the text going into the Buffer update
+      callback - the callback function
+*/
+const bufferRequest = (tweet, callback) => {
+  //Buffer request body
+  let data = {
+    text: tweet,
+    shorten: true,
+    now: false,
+    top: false,
+    access_token: BUFFER_ACCESS_TOKEN
+  };
+
+  getBufferProfileIDs( (ids) => {
+    data.profile_ids = ids;
+    console.log(data);
+    requestModule.post({ url:"https://api.bufferapp.com/1/updates/create.json", form: data }, (err, res, body) => {
+      if (err){ //if it's an error
+        callback(err); //send the error to the callback
+      }
+      else if(res.statusCode != 200){
+        console.log('bufferRequest(): Something happened.');
+        callback(null, res, body);
+      }
+      else{ //log the successful Buffer request
+        console.log('bufferRequest(): Upload to Buffer successful.');
+        callback(null, res, body); //then send the parameters to the callback
+      }
+    });
+  });
+};
+
+/*
+slackResponse method
+  once the slash command is received and succesfully posted to Buffer, a 'Thank you!' message will be sent back to the user.
+    parameters:
+      req - request object
+      res - response object
+      callback - the callback function
+*/
+const slackResponse = (req, res, callback) => {
+  var userName = req.body.user_name;
+  var payload = {
+    text: 'Thanks, ' + userName + '! Your message was added to Buffer.'
+  };
+
+  if ( userName !== 'slackbot')
+  { //send the response back to the Slack channel
+    return res.status(200).json(payload);
+    callback();
+  }
+  else {
+    return res.status(200).end();
   }
 };
 
@@ -35,19 +130,19 @@ const PROFILE_IDS = process.env.BUFFER_PROFILE_IDS ||
   this is the endpoint that's touched when a slash command is fired.
 */
 app.post('/command', (request, response) => {
-  if( request.code === SLACK_TOKEN )
+  if( request.body.token === SLACK_TOKEN )
   {
-    bufferModule.bufferRequest(request.body.text, (err, res, body) => {
+    bufferRequest(request.body.text, (err, res, body) => {
       if( err ){
         console.error('Upload failed: ', err);
       }
       else if( res.statusCode !== 200 ){
-        console.log('Something happened. Status Code: %s | %s', res.StatusCode, body);
+        console.log('Status Code: %s', res.StatusCode);
+        console.log(body);
       }
       else{
-        console.log("Upload successful. Server response: ", body);
-        slackModule.slackResponse(request, response, () => {
-          console.log('thank you message posted to Slack.');
+        slackResponse(request, response, () => {
+          console.log('app.post(): thank you message posted to Slack.');
         });
       }
     });
@@ -58,6 +153,13 @@ app.post('/command', (request, response) => {
   }
 });
 
+app.get('/command', (request, response) => {
+  if( response.body.ssl_check === 1 ){
+    response.status(200);
+    response.end;
+  }
+});
+
 app.listen(PORT, () => {
-  console.log('app is running on port %s', PORT);
+  console.log('app.listen(): app is running on port %s', PORT);
 });
